4ea9119674995e28c8a4790274694bade72a65e0
Matthew Castoreno <matthew.castoreno@yahoo.com>
2022-07-16 13:23:51 -0500

    Longitude and Latitude needs to go into forecast

--- script.js
+++ script.js
@@ -3,39 +3,50 @@
 // http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
 
 // var testGeo = `https://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=${apiKey}`
-// Forecast for the next 16 days
-var forescast = `api.openweathermap.org/data/2.5/forecast/daily?lat={lat}&lon={lon}&cnt={cnt}&appid={API key}`
-// Need an api for the current weather API KEY 1b58babad9d1e2a3475a0cb127e62e77
-// var currentWeather = `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=${apiKey}`
+// Need an api for the current weather 
+// var currentWeather = `api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`
 
 
-// Link the search button
-var searchButton = document.getElementById('searchButton');
-searchButton.addEventListener('click', getApi);
 
-let lat;
-let lon;
+
 
 //  API key 
 var apiKey = '40560cd406f9b5804909ab5d09157282'
+var lat;
+var lon;
+// Link the search button
+var searchButton = document.getElementById('searchButton');
+searchButton.addEventListener('click', getLatLon);
 
-function getApi() {
+function getLatLon() {
 
     // Create variables for information from user input
-    var userSearch = document.querySelector('#searchedCity').value;
-
+    var userSearch = document.getElementById('searchedCity').value;
     // Need coordinates for the searched results
     var geoCodeApi = `https://api.openweathermap.org/geo/1.0/direct?q=${userSearch}&limit=5&appid=${apiKey}`
 
     fetch(geoCodeApi)
-        .then(function (response) {
+        .then(function(response){
             return response.json();
-        }).then(function (data) {
-            console.log(data);
-            let lat = data[0].lat
-            let lon = data[0].lon
-            // console.log(lat);
-            // console.log(lon);
+        }).then(function(data){
+            // console.log(data);
+            lat = data[0].lat
+            lon = data[0].lon
+            console.log("lat", data[0].lat);
+            console.log("lon", data[0].lon);
+            currentForecast();
         })
+};
 
-}
+function currentForecast() {
+
+    var forecast = `https://api.openweathermap.org/data/2.5/forecast/daily?lat=${lat}&lon=${lon}&appid=${apiKey}`
+    
+    fetch(forecast)
+        .then(function(response){
+            return response.json();
+        }).then(function(data){
+            console.log(data);
+        })
+};
+
