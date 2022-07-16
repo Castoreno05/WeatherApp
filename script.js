2c2f2147d4e73a8727bf463a05459b7f1fac78c9
Matthew Castoreno <matthew.castoreno@yahoo.com>
2022-07-16 09:44:40 -0500

    getApi function

--- script.js
+++ script.js
@@ -7,15 +7,23 @@
 var forescast = `api.openweathermap.org/data/2.5/forecast/daily?lat={lat}&lon={lon}&cnt={cnt}&appid={API key}`
 // Need an api for the current weather API KEY 1b58babad9d1e2a3475a0cb127e62e77
 // var currentWeather = `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=${apiKey}`
+
+
 // Link the search button
 var searchButton = document.getElementById('searchButton');
 searchButton.addEventListener('click', getApi);
 
+let lat;
+let lon;
+
+//  API key 
+var apiKey = '40560cd406f9b5804909ab5d09157282'
+
 function getApi() {
+
     // Create variables for information from user input
-    var userSearch = document.getElementById('searchedCity').value
-    //  API key 
-    var apiKey = '40560cd406f9b5804909ab5d09157282'
+    var userSearch = document.querySelector('#searchedCity').value;
+
     // Need coordinates for the searched results
     var geoCodeApi = `https://api.openweathermap.org/geo/1.0/direct?q=${userSearch}&limit=5&appid=${apiKey}`
 
@@ -24,17 +32,10 @@ function getApi() {
             return response.json();
         }).then(function (data) {
             console.log(data);
-            // var lat = data[0].lat
-            // var lon = data[0].lon
+            let lat = data[0].lat
+            let lon = data[0].lon
             // console.log(lat);
             // console.log(lon);
         })
+
 }
-
-
-// fetch()
-// .then(function(response){
-//     return response.json();
-// }).then(function(data){
-//     console.log(data);
-// })
